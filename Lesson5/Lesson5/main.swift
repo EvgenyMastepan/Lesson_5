02bfec9e18cc1405040682e3346f0512e1c08372
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-10-03 10:58:54 +0700

    Задание 2-3

--- Lesson5/Lesson5/main.swift
+++ Lesson5/Lesson5/main.swift
@@ -44,38 +44,117 @@ var errorCode: Bool = true
 //    } else { errorCode = false }
 //} else { errorCode = false }
 //if !errorCode { print("Ошибка ввода данных.") }
+//
+////=============================================
+////              2. Калькулятор
+////=============================================
+//errorCode = true // Сброс флага ошибок
+//
+//func inputNumber()->Double {
+//    if let number = readLine() {
+//        if Double(number) != nil {
+//            return Double(number) ?? 0
+//        } else {
+//            errorCode = false
+//            return 0
+//        }
+//    } else {
+//        errorCode = false
+//        return 0
+//    }
+//}
+//
+//print("Введите первое число:")
+//let firstNumber = inputNumber()
+//print("Введите второе число:")
+//let secondNumber = inputNumber()
+//print ("Введите операцию + - * / ")
+//if let operation = readLine() {
+//    switch operation {
+//    case "+": print(firstNumber + secondNumber)
+//    case "-": print(firstNumber - secondNumber)
+//    case "*": print(firstNumber * secondNumber)
+//    case "/": print(firstNumber / secondNumber)
+//    default: errorCode = false
+//    }
+//} else { errorCode = false }
+//if !errorCode { print("Ошибка ввода данных.") }
+
+////=============================================
+////                        3. Права доступа
+////=============================================
+//errorCode = true // Сброс флага ошибок
+//
+//print ("Введите уровень доступа")
+//if let accessLevel = readLine() {
+//    switch accessLevel {
+//    case "admin": print("chmod: Полный доступ с правами управления пользователями.")
+//    case "root": print("chmod: Полный доступ без ограничений.")
+//    case "user": print("chmod: Ограниченный доступ.")
+//    case "guest": print("chmod: Ограниченный доступ только на чтение.")
+//    default: print("В доступе отказано.")
+//    }
+//} else { errorCode = false }
+//if !errorCode { print("Ошибка ввода данных.") }
+
+////=============================================
+////              1. Светофор
+////=============================================
+//errorCode = true // Сброс флага ошибок
+//
+///*Задача 1: Создайте перечисление TrafficLight, которое описывает состояния светофора: red, yellow, green. Напишите функцию, которая принимает состояние светофора и выводит соответствующее действие:
+// Если red, то вывести "Стоп".
+// Если yellow, то вывести "Приготовьтесь".
+// Если green, то вывести "Можно идти".*/
+//
+//enum TrafficLight {
+//    case red
+//    case yellow
+//    case green
+//}
+//
+//func roadControl(state: TrafficLight) {
+//    switch state {
+//    case .red:
+//       print("Стоп")
+//    case .yellow:
+//       print("Приготовьтесь")
+//    case .green:
+//       print("Можно идти")
+//   }
+//}
+//
+//roadControl(state: .yellow)
 
 //=============================================
-//              2. Калькулятор
+//              2. Рабочий календарь
 //=============================================
 errorCode = true // Сброс флага ошибок
 
-func inputNumber()->Double {
-    if let number = readLine() {
-        if Double(number) != nil {
-            return Double(number) ?? 0
-        } else {
-            errorCode = false
-            return 0
-        }
-    } else {
-        errorCode = false
-        return 0
-    }
+/* Задача 2: Создайте перечисление Weekday, которое описывает дни недели: monday, tuesday, wednesday, thursday, friday, saturday, sunday. Напишите функцию, которая принимает день недели и выводит, является ли этот день рабочим или выходным.*/
+
+enum WeekDay {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
 }
 
-print("Введите первое число:")
-let firstNumber = inputNumber()
-print("Введите второе число:")
-let secondNumber = inputNumber()
-print ("Введите операцию + - * / ")
-if let operation = readLine() {
-    switch operation {
-    case "+": print(firstNumber + secondNumber)
-    case "-": print(firstNumber - secondNumber)
-    case "*": print(firstNumber * secondNumber)
-    case "/": print(firstNumber / secondNumber)
-    default: errorCode = false
-    }
-} else { errorCode = false }
-if !errorCode { print("Ошибка ввода данных.") }
+func workCalendar(state: WeekDay) {
+    switch state {
+    case .saturday,
+         .sunday:
+       print("Выходной")
+    case .monday,
+         .thursday,
+         .tuesday,
+         .friday,
+         .wednesday:
+       print("Рабочий день")
+   }
+}
+
+workCalendar(state: .sunday)
