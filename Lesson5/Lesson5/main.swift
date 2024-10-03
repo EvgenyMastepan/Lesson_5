f750f0e3c3c3b63903c937b5b138c29405b2239c
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-10-03 10:01:01 +0700

    Второе задание.

--- Lesson5/Lesson5/main.swift
+++ Lesson5/Lesson5/main.swift
@@ -6,21 +6,76 @@
 //
 
 import Foundation
-var errorCode: Bool = false
-//=============================================
-//              1.
-//=============================================
+var errorCode: Bool = true
+////=============================================
+////                             1. Знак числа
+////=============================================
+//
+//print("Введите целое число")
+//if let tempNumber = readLine() {
+//    if Int(tempNumber) != nil {
+//        let number = Int(tempNumber) ?? 0
+//        switch number {
+//          case ..<0: print ("Число меньше нуля.")
+//          case 0: print ("Число равно нулю.")
+//          case 0... : print ("Число больше нуля.")
+//          default: print("Число вне диапазона.")
+//        }
+//    } else { errorCode = false }
+//} else { errorCode = false }
+// if !errorCode { print("Ошибка ввода данных.") }
 
-print("Введите целое число")
-if let tempNumber = readLine() {
-    if Int(tempNumber) != nil {
-        let number = Int(tempNumber) ?? 0
-        switch number {
-        case ..<0: print ("Число меньше нуля.")
-        case 0: print ("Число равно нулю.")
-        case 0... : print ("Число больше нуля.")
-        default: print("Число вне диапазона.")
+////=============================================
+////                       1bis. Вивальди. "Времена года."
+////=============================================
+//errorCode = true
+//
+//print("Введите номер месяца")
+//if let tempMonth = readLine() {
+//    if Int(tempMonth) != nil {
+//        let month = Int(tempMonth) ?? 0
+//        switch month {
+//            case 1,2,12: print("Это зима.")
+//            case 3...5: print("Это весна.")
+//            case 6...8: print("Это лето.")
+//            case 9...11: print("Это зима.")
+//        default: print("Время года неизвестное науке.")
+//        }
+//    } else { errorCode = false }
+//} else { errorCode = false }
+//if !errorCode { print("Ошибка ввода данных.") }
+
+//=============================================
+//              2. Калькулятор
+//=============================================
+errorCode = true // Сброс флага ошибок
+
+func inputNumber()->Double {
+    if let number = readLine() {
+        if Double(number) != nil {
+            return Double(number) ?? 0
+        } else {
+            errorCode = false
+            return 0
         }
-    } else { errorCode = false }
-} else { errorCode = false }
+    } else {
+        errorCode = false
+        return 0
+    }
+}
 
+print("Введите первое число:")
+let firstNumber = inputNumber()
+print("Введите второе число:")
+let secondNumber = inputNumber()
+print ("Введите операцию + - * / ")
+if let operation = readLine() {
+    switch operation {
+    case "+": print(firstNumber + secondNumber)
+    case "-": print(firstNumber - secondNumber)
+    case "*": print(firstNumber * secondNumber)
+    case "/": print(firstNumber / secondNumber)
+    default: errorCode = false
+    }
+} else { errorCode = false }
+if !errorCode { print("Ошибка ввода данных.") }
